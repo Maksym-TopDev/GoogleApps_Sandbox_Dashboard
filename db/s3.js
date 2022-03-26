b636e257e410660808995f025ce47f12c377f3f2
Alvarian <alvarivan88@gmail.com>
2022-03-26 07:36:22 -0400

    remove variable declaration for bucket upload

--- db/s3.js
+++ db/s3.js
@@ -35,35 +35,35 @@ async function createOrUpdate(files, fields, pgCb) {
       const corePath = file.name === "core" && `${title}/${version}`;
       const iconPath = file.name === "icon" && `${title}/media/${file.name}`;
       
-      const response = await s3.upload({
+      s3.upload({
         Bucket: BUCKET_NAME, // pass your bucket name
         Key: corePath || iconPath,
         Body: file.data,
         ContentType: file.type,
         CacheControl: "max-age=0"
+      }, function(err, data) {
+        console.log("PRINT FILE:", file);
+        if (err) {
+          errMsg.push(err)
+        } else {
+          console.log('Successfully uploaded data', data);
+        }
       });
-      console.log(response.service.config)
-      // if (!response.location) {
-      //   errMsg.push(response)
-      //   return;
-      // }
-
-      // console.log(`File uploaded successfully at ${response}`);
     });
 
     if (errMsg.length) throw errMsg;
 
-    // await pgCb({
-    //   app_type: projectType, 
-    //   deployed_url: website, 
-    //   description, 
-    //   game_file: (files[0]?.data) ? `${S3_ROOT_URL}/${title}/${version}` : null, 
-    //   git_url: repository, 
-    //   icon_file: (files[1]?.data) ? `${S3_ROOT_URL}/${title}/media/${files[1].name}` : null, 
-    //   secret_key: secret, 
-    //   title,
-    //   version
-    // });
+    await pgCb({
+      app_type: projectType, 
+      deployed_url: website, 
+      description, 
+      game_file: (files[0]?.data) ? `${S3_ROOT_URL}/${title}/${version}` : null, 
+      git_url: repository, 
+      icon_file: (files[1]?.data) ? `${S3_ROOT_URL}/${title}/media/${files[1].name}` : null, 
+      secret_key: secret, 
+      title,
+      version
+    });
   } catch (err) {
     throw "S3 upload failed: "+err;
   }
